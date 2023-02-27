ae43af82c333f1a3bfa05c65489aad8ee1113b4c
Kittinat's Mac <>
2023-02-27 15:22:03 +0700

    - update redux store persistConfig.

--- src/store.js
+++ src/store.js
@@ -1,18 +1,31 @@
-import { createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialState = {
   customerData: null,
 };
 
-function rootReducer(state = initialState, action) {
+const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SAVE_CUSTOMER_DATA":
+    case 'SAVE_CUSTOMER_DATA':
       return { ...state, customerData: action.payload };
     default:
       return state;
   }
-}
+};
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
